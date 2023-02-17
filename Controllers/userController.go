2bab7e4da65544495efa9fb37994df2f8290571b
Frankie <vanyongqi@gmail.com>
2023-02-17 17:23:32 +0800

    add login and register module

--- Controllers/userController.go
+++ Controllers/userController.go
@@ -6,85 +6,16 @@ import (
 	"dousheng-backend/Databases/DAO"
 	"dousheng-backend/Middlewares"
 	"dousheng-backend/Models"
+	"dousheng-backend/Utils"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
-	"golang.org/x/crypto/bcrypt"
 	"net/http"
+	"time"
 )
-
-/*
-	demo代码，注册、登录、个人信息三个函数，定义了一个map[string]User变量 做测试
-TEST code start
-// user data will be cleared every time the server starts
-// test data: username=zhanglei, password=douyin
-func Register(c *gin.Context) {}
-func Login(c *gin.Context) {}
-func UserInfo(c *gin.Context) {}
-*/
-/*
-var usersLoginInfo = map[string]common.User{"fanyongqi": {
-	Id:            1,
-	Name:          "fanyongqi",
-	Password:      "123456",
-	FollowCount:   10,
-	FollowerCount: 5,
-	IsFollow:      true,
-}}
-var userIdSequence = int64(1)
-
-
-func Register(c *gin.Context) {
-	username := c.Query("username")
-	password := c.Query("password")
-
-	token := username + password
-
-	if _, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: Response{StatusCode: 1, StatusMsg: "User already exist"},
-		})
-	} else {
-		atomic.AddInt64(&userIdSequence, 1)
-		newUser := common.User{
-			Id:   userIdSequence,
-			Name: username,
-		}
-		usersLoginInfo[token] = newUser
-		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: Response{StatusCode: 0},
-			UserId:   userIdSequence,
-			Token:    username + password,
-		})
-	}
-}
-
-//TEST is over.
-*/
-
-/**************** var block ***********************/
-
-type RegisterLoginRequest struct {
-	UserName string `json:"username" binding:"required,min=4,max=16" form:"username"`
-	Password string `json:"password" binding:"required,min=4,max=16" form:"password"`
-}
-
-/***********************func block*********************************/
-
-func BcryptEncode(str string) string {
-	bcryptPassword, err := bcrypt.GenerateFromPassword([]byte(str), bcrypt.DefaultCost)
-	if err != nil {
-		logrus.Info("调用bcrypt,加密异常，HASH编码失败:%w", err)
-	}
-	return string(bcryptPassword)
-}
-
-func VerifyPassword(sourcePwd, hashPwd string) bool {
-	return bcrypt.CompareHashAndPassword([]byte(hashPwd), []byte(sourcePwd)) == nil
-}
 
 func PostRegisterUser(c *gin.Context) {
 	db := Databases.DatabaseSession()
-	var req RegisterLoginRequest
+	var req common.RegisterLoginRequest
 
 	// s1 verify parameters is valid
 	if err := c.ShouldBindQuery(&req); err != nil {
@@ -100,17 +31,17 @@ func PostRegisterUser(c *gin.Context) {
 	// s2 verify user is already registered or not
 	if DAO.GetUserByName(db, req.UserName) != nil {
 		logrus.Warn("用户名重复，用户注册失败！the name already exist in database")
-		c.JSON(http.StatusOK, common.Response{
+		c.JSON(http.StatusBadRequest, common.Response{
 			StatusCode: 2,
 			StatusMsg:  "用户名重复，用户注册失败！the name already exist in database",
 		})
-		c.JSON(200, req.UserName)
+		c.JSON(2, req.UserName)
 		return
 	}
 	// s3 create user from GROM by username and password
 	registerUser := DAO.CreateUser(db, &Models.User{
 		Name:     req.UserName,
-		Password: BcryptEncode(req.Password),
+		Password: Utils.BcryptEncode(req.Password),
 		Content:  "",
 	})
 	// s4 generate token from middleware layer
@@ -136,7 +67,7 @@ func PostRegisterUser(c *gin.Context) {
 
 func PostLoginUser(c *gin.Context) {
 	db := Databases.DatabaseSession()
-	var req RegisterLoginRequest
+	var req common.RegisterLoginRequest
 
 	// s1 verify parameters is valid
 	if err := c.ShouldBindQuery(&req); err != nil {
@@ -161,7 +92,7 @@ func PostLoginUser(c *gin.Context) {
 	}
 
 	// s4 check user password is existed or not
-	if VerifyPassword(req.Password, loginUser.Password) == false {
+	if Utils.VerifyPassword(req.Password, loginUser.Password) == false {
 		c.JSON(http.StatusExpectationFailed, common.Response{
 			StatusCode: http.StatusExpectationFailed,
 			StatusMsg:  "用户密码错误",
@@ -194,5 +125,26 @@ func PostLoginUser(c *gin.Context) {
 }
 
 func GetUserInfo(c *gin.Context) {
+	db := Databases.DatabaseSession()
+	var loginUser *Models.User
+	token := c.Query("token")
+	if token != "" {
+		loginUserID, loginUserName, err := Middlewares.ParseToken(c, token)
+		if err != nil {
+			return
+		}
+		loginUser, err = DAO.GetUserByID(db, loginUserID)
+		logrus.Info(string(loginUserName) + " 用户尝试登录 in" + time.Now().String())
+		if err != nil {
+			c.JSON(http.StatusExpectationFailed, common.Response{
+				StatusCode: http.StatusExpectationFailed,
+				StatusMsg:  "用户不存在",
+			})
+			return
+		}
+
+	}
+
+	c.JSON(http.StatusOK, loginUser)
 
 }
