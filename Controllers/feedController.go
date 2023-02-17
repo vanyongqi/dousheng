dc488810f7a455957b1d0d5fad20237fcc19963a
Frankie <vanyongqi@gmail.com>
2023-02-17 14:55:29 +0800

    remove logrus test

--- Controllers/feedController.go
+++ Controllers/feedController.go
@@ -2,11 +2,9 @@ package Controllers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
 )
 
 func GetFeed(c *gin.Context) {
-	logrus.Info("Get Feed!!!!!!!!!!!!!!!!!!!")
 
 	c.JSON(200, gin.H{"123": "123"})
 }
